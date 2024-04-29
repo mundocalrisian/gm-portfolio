f858811a9e2108ac3fc68479afee0e2e268aac09
mundocalrisian <greg.munden@gmail.com>
2024-04-29 23:55:15 +0100

    add about me section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,11 @@ export default function About () {
     return (
         <section className="md:w-[350px] lg:w-[600px]  xl:w-[900px] sm:mt-0 md:mt-4">
             <h2 className="font-medium mb-4 sm:block md:hidden"> About me</h2>
-            <p>Croque monsieur emmental melted cheese. Paneer mozzarella cottage cheese cottage cheese cheesecake boursin halloumi macaroni cheese. Queso macaroni cheese cheeseburger edam croque monsieur queso port-salut blue castello. Paneer cow cut the cheese bocconcini camembert de normandie swiss bocconcini say cheese. Cheese and biscuits parmesan babybel stinking bishop roquefort port-salut cheddar fromage frais. Cottage cheese bavarian bergkase cow rubber cheese stilton say cheese st. agur blue cheese queso. Fondue pecorino cheese triangles lancashire cheese triangles cheddar jarlsberg.</p>
+            <h3 className="text-xl font-medium mb-4">Hello there, I'm Greg</h3>
+            <p> Recently, I've graduated from a 3 month Bootcamp with <a href="https://northcoders.com/"  target="_blank" rel="noreferrer noopener" className="font-medium text-zinc-700 dark:text-orange-200 hover:text-orange-500 dark:hover:text-orange-500">Northcoders</a>, marking the beginning of a journey to realise a long term dream of mine to become a <span className="font-medium text-zinc-700 dark:text-orange-200">Software Developer</span>.</p><br/>
+            <p>I have transitioned from a successful career in retail, helping customers and working with some of my biggest passions, including music, Apple computers and mountain bikes. And so, it is with these communication skills that I'm excited to start a new journey into the technology industry.</p><br/>
+            <p>When not sitting in front of a computer, I'm happiest outdoors, surrounded by epic scenery. I love hiking and mountain biking, and through these, I've been lucky to explore some amazing landscapes in countries such as Iceland, Chile and Greenland. Photography has always fascinated me, and you can see some of mine here on this site, as well as the Tumblr blog I wrote whilst living in Iceland.</p><br/>
+            <p>If you'd like to know more, please feel free to reach out and contact me.</p>
         </section>
     )
 }
