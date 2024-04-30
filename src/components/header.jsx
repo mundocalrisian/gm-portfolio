9691170ac8d5e9bd676c2c79c4d073a2eb863e29
mundocalrisian <greg.munden@gmail.com>
2024-04-30 17:57:42 +0100

    add light bulb to header for smaller screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,7 @@ import envelope from '../assets/contact-logos/envelope.svg'
 import instagram from '../assets/contact-logos/instagram.svg'
 import tumblr from '../assets/contact-logos/square-tumblr.svg'
 import meHeadshot from "../assets/me-headshot.jpg"
+import bulb from "../assets/lightbulb.svg"
 import { DarkContext } from '../context/dark'
 import { useContext, useEffect } from 'react'
 
@@ -51,19 +52,24 @@ export default function Header () {
                     })}
                 </ul>
             </section>
-            <div className='ml-4 mt-4 mb-4'>
+            
+            {/* <p className="text-sm md:pl-8"></p> */}
+            <div className="ml-4 mb-2 w-5/6 flex flex-end items-center">
+                <label className="border-2 rounded-lg border-zinc-800/20 dark:border-orange-50/50 p-2 hover:cursor-pointer md:hidden" htmlFor="dark-mode-toggle-switch-footer">
+                    <img src={bulb} width="15" className={dark !=="true" ? 'filter-zinc': 'filter-orange-50'}/>
+                </label>
                 <input 
                     type="checkbox" 
                     className=""
                     name="dark-mode-toggle-switch"
-                    id="dark-mode-toggle-switch"
+                    id="dark-mode-toggle-switch-footer"
                     onClick={toggleDark} 
                     defaultChecked={true}
+                    hidden
                 />
-                <label className="ml-2" htmlFor="dark-mode-toggle-switch">
-                    Toggle Dark Mode
-                </label>
+                <p onClick={toggleDark} className="ml-4 text-sm hover:cursor-pointer md:hidden">Click  {dark === "true" ? "to see the light" : "for some darkness"}</p>
             </div>
+            
         </header>
     )
 }
