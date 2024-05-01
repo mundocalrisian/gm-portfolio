bee9318052a3e4e070c46882c9cfd981d7db2474
mundocalrisian <greg.munden@gmail.com>
2024-05-01 18:48:46 +0100

    start gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,9 +1,16 @@
+import image1 from "../assets/gallery/IMG_0269.jpg"
+import image2 from "../assets/gallery/IMG_3451.jpg"
 
 export default function Gallery () {
 
     return (
         <section className="mb-8 md:w-3/5 lg:w-2/3  xl:w-[900px] pl-4  lg:pl-0 sm:mt-0 md:mt-4">
-            <p className="text-left" >Ricotta pecorino mozzarella. Ricotta paneer say cheese boursin mascarpone cheese on toast chalk and cheese chalk and cheese. Who moved my cheese bavarian bergkase cheese triangles goat blue castello cheese on toast squirty cheese swiss. Say cheese camembert de normandie babybel cheese strings cheesy feet who moved my cheese lancashire stilton. Red leicester queso say cheese mozzarella cheesecake fromage frais bocconcini cheeseburger. Who moved my cheese dolcelatte chalk and cheese stilton mozzarella edam.</p>
+            {/* <p className="text-center" >Here are a few of my favourite images that I've taken on my adventures</p> */}
+            <div className="flex xl:flex-row flex-wrap justify-around md:mt-4">
+                <img src={image1} className="xl:max-w-96 p-3 border bg-zinc-50 shadow-lg mt-2"/>
+                <img src={image2} className="xl:max-w-96 p-3 border bg-zinc-50 shadow-lg mt-4"/>
+                
+            </div>
         </section>
     )
 
