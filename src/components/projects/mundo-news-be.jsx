7901f1d765358ff3231fd831d4a7e14706f17e36
mundocalrisian <greg.munden@gmail.com>
2024-05-02 08:07:00 +0100

    alter project descriptions

--- src/components/projects/mundo-news-be.jsx
+++ src/components/projects/mundo-news-be.jsx
@@ -22,7 +22,7 @@ export default function MundoNewsBe () {
     return (
         <section className="bg-zinc-200/30 border border-orange-50 hover:border-slate-400 hover:bg-sky-200/10 dark:bg-zinc-900/20 dark:border-zinc-800 dark:hover:border dark:hover:border-gray-700 dark:hover:bg-transparent dark:hover:bg-gradient-to-b from-slate-800/50 to-gray-700/30 rounded-3xl mb-6 p-6">
             <h2 className="text-xl font-medium pb-4 dark:text-white">Mundo News - Backend</h2>
-            <p>A RESTful API backend project built using JavaScript and Express.js to interact with a PostgreSQL database. THis project was constructed and tested within 5 days as part of the backend phase of my Northcoders Software Development Bootcamp.
+            <p>A RESTful API backend project built using JavaScript and Express.js to interact with a PostgreSQL database. This project was constructed and tested within 5 days as part of the backend phase of my Northcoders Software Development Bootcamp.
             </p>
             <div className="flex justify-center">
                 <a href="https://nc-news-b3bj.onrender.com/api" target="_blank" rel="noreferrer noopener" className="mt-4 mb-2 mr-2 p-2 border rounded-lg bg-orange-200/50 dark:bg-transparent border-orange-500 hover:bg-orange-500 dark:hover:bg-orange-500 hover:text-white xs:w-1/2 w-2/5 md:w-1/3 lg:w-1/4">View API</a>
--- src/components/projects/mundo-weather.jsx
+++ src/components/projects/mundo-weather.jsx
@@ -21,7 +21,7 @@ export default function MundoWeather () {
     return (
         <section className="bg-zinc-200/30 border border-orange-50 hover:border-slate-400 hover:bg-sky-200/10 dark:bg-zinc-900/20 dark:border-zinc-800 dark:hover:border dark:hover:border-gray-700 dark:hover:bg-transparent dark:hover:bg-gradient-to-b from-slate-800/50 to-gray-700/30 rounded-3xl mb-6 p-6">
             <h2 className="text-xl font-medium pb-4 dark:text-white">Mundo Weather</h2>
-            <p>My first React web apploication, exploring data visualisation from open-source APIs to show the weather forecast for a chosen location.
+            <p>My first React web application, exploring data visualisation from open-source APIs, showing the weather forecast for a chosen location.
             </p>
             <div className="flex justify-center">
                 <a href="https://mundo-weather.netlify.app/" target="_blank" rel="noreferrer noopener" className="mt-4 mb-2 mr-2 p-2 border rounded-lg bg-orange-200/50 dark:bg-transparent border-orange-500 hover:bg-orange-500 dark:hover:bg-orange-500 hover:text-white xs:w-1/2 w-2/5 md:w-1/3 lg:w-1/4">View Site</a>
