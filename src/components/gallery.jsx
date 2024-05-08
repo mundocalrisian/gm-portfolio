dffd2254ad143d582811688dc7aeb8b3be8921db
mundocalrisian <greg.munden@gmail.com>
2024-05-08 22:06:48 +0100

    replace sample images with my own

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,39 +1,39 @@
 import { useEffect, useState } from "react"
-import { getAllImages } from "../utils/api"
+// import { getAllImages } from "../utils/api"
 import imageData from "../assets/images.json"
 import PhotoAlbum from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox"
 import "yet-another-react-lightbox/styles.css";
+import CloudImage from "../utils/api";
+import { shuffle } from "../utils/utils";
 
 
 export default function Gallery () {
     const [imageList, setImageList] = useState(imageData.resources)
     const [index, setIndex] = useState(-1)
+    const [slides, setSlides] = useState([])
 
     useEffect(() => {
-        // const fetchImages = async () => {
-        //     const allImages = await getAllImages()
-        //     setImageList(allImages.resources)
-        //     // console.log(allImages, "-----allImages in useEffect");
-        // }
-        // fetchImages()
+        const portfolioImages = imageData.resources.reduce((images, image) => {
+            if (image.folder.includes('portfolio')) {
+                images.push(image)
+            }
+            return images
+        }, [])
+        // console.log(portfolioImages, "-----portfolioImages");
+        shuffle(portfolioImages)
+        
+        const slidesToSet = portfolioImages.map((image) => {
+                return (
+                    {src: image.url, width: image.width, height: image.height}
+                )
+        })
+        setSlides(slidesToSet)
     }, [])
-
-    const slides = imageData.resources.map((image) => {
-        return (
-            {src: image.url, width: image.width, height: image.height}
-        )
-    })
 
     return (
         <section className="mb-8 md:w-3/5 lg:w-2/3  xl:w-[900px] pl-4  lg:pl-0 sm:mt-0 md:mt-4">
-            {/* <p className="text-center" >Here are a few of my favourite images that I've taken on my adventures</p> */}
-            {/* <div className="flex xl:flex-row flex-wrap justify-around md:mt-4">
-                <img src={image1} alt="a frosty still day besides a lake" className="xl:max-w-96 p-3 border bg-zinc-50 shadow-lg mt-2"/>
-                <img src={image2} alt="a sunset looking down a valley" className="xl:max-w-96 p-3 border bg-zinc-50 shadow-lg mt-4"/>
-                
-            </div> */}
-            <div>
+            <div className="p-4">
                 <PhotoAlbum 
                     layout="rows" 
                     photos={slides}
